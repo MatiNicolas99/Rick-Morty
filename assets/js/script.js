1e3868f827bf1b1771d0d36f17ca5fcff20d1bc7
MatiNicolas99 <103612186+MatiNicolas99@users.noreply.github.com>
2022-06-29 12:12:39 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,69 @@
+const charactersContainer = document.querySelector(".characters-container");
+
+
+const traerDatos = async () => {
+
+    try {
+      let datos_globales = await axios(`https://rickandmortyapi.com/api/character`);
+      let datos_personajes = datos_globales.data.results
+
+      datos_personajes.map(personaje => new Personaje(personaje));
+
+     } catch (error) {
+        throw error;
+     }     
+};
+
+
+
+class Personaje {
+  constructor({ id, name, image, gender, species}) {
+    this.id = id;
+    this.name = name;
+    this.image = image;
+    this.gender = gender;
+    this.species = species;
+
+    const card = document.createElement("div");
+    card.classList.add("characters-card");
+
+    const cardImage = document.createElement("div");
+    cardImage.classList.add("img-container");
+
+    const characterPhoto = document.createElement("img");
+    characterPhoto.src = this.image;
+
+    const characterId = document.createElement("p");
+    characterId.classList.add("character-id");
+    characterId.textContent = `ID: ${this.id}`;
+
+    const cardName = document.createElement("p");
+    cardName.classList.add("name");
+    cardName.textContent =
+      this.name.charAt(0).toUpperCase() + this.name.slice(1);
+
+    const cardGender = document.createElement("p");
+    cardGender.classList.add("gender");
+    cardGender.textContent = this.gender;
+
+    const cardSpecies = document.createElement("h2");
+    cardSpecies.classList.add("species");
+    cardSpecies.textContent = this.species;
+
+
+    charactersContainer.appendChild(card);
+
+    cardImage.appendChild(characterPhoto);
+    card.appendChild(cardImage);
+    card.appendChild(characterId);
+    card.appendChild(cardName);
+    card.appendChild(cardGender);    
+    card.appendChild(cardSpecies);
+    
+   
+  }
+}
+
+window.onload = async () => {
+  await traerDatos();
+}
