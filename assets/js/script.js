6d7ffa423952ede314f9c701392fb10c34546e57
MatiNicolas99 <103612186+MatiNicolas99@users.noreply.github.com>
2022-07-02 22:15:16 -0400

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,55 +14,69 @@ const traerDatos = async () => {
      }     
 };
 
-
-
 class Personaje {
-  constructor({ id, name, image, gender, species}) {
-    this.id = id;
-    this.name = name;
-    this.image = image;
-    this.gender = gender;
-    this.species = species;
-
-    const card = document.createElement("div");
-    card.classList.add("characters-card");
-
-    const cardImage = document.createElement("div");
-    cardImage.classList.add("img-container");
-
-    const characterPhoto = document.createElement("img");
-    characterPhoto.src = this.image;
-
-    const characterId = document.createElement("p");
-    characterId.classList.add("character-id");
-    characterId.textContent = `ID: ${this.id}`;
-
-    const cardName = document.createElement("p");
-    cardName.classList.add("name");
-    cardName.textContent =
-      this.name.charAt(0).toUpperCase() + this.name.slice(1);
-
-    const cardGender = document.createElement("p");
-    cardGender.classList.add("gender");
-    cardGender.textContent = this.gender;
-
-    const cardSpecies = document.createElement("h2");
-    cardSpecies.classList.add("species");
-    cardSpecies.textContent = this.species;
-
-
-    charactersContainer.appendChild(card);
-
-    cardImage.appendChild(characterPhoto);
-    card.appendChild(cardImage);
-    card.appendChild(characterId);
-    card.appendChild(cardName);
-    card.appendChild(cardGender);    
-    card.appendChild(cardSpecies);
-    
-   
+  constructor({id, name, image, gender, species}) {
+    this.__id = () => id;
+    this.__name = () => name;
+    this.__image = () => image;
+    this.__gender = () => gender;
+    this.__species = () => species;
+    this.show();
   }
-}
+
+    get id() {
+      return this.__id()
+    }
+    get name() {
+      return this.__name()
+    }
+    get image() {
+      return this.__image()
+    }
+    get gender() {
+      return this.__gender()
+    }
+    get species() {
+      return this.__species()
+    }
+    show(){
+      const card = document.createElement("div");
+      card.classList.add("characters-card");
+  
+      const cardImage = document.createElement("div");
+      cardImage.classList.add("img-container");
+  
+      const characterPhoto = document.createElement("img");
+      characterPhoto.src = this.image;
+  
+      const characterId = document.createElement("p");
+      characterId.classList.add("character-id");
+      characterId.textContent = `ID: ${this.id}`;
+  
+      const cardName = document.createElement("p");
+      cardName.classList.add("name");
+      cardName.textContent =
+        this.name.charAt(0).toUpperCase() + this.name.slice(1);
+  
+      const cardGender = document.createElement("p");
+      cardGender.classList.add("gender");
+      cardGender.textContent = this.gender;
+  
+      const cardSpecies = document.createElement("h2");
+      cardSpecies.classList.add("species");
+      cardSpecies.textContent = this.species;
+  
+  
+      charactersContainer.appendChild(card);
+  
+      cardImage.appendChild(characterPhoto);
+      card.appendChild(cardImage);
+      card.appendChild(characterId);
+      card.appendChild(cardName);
+      card.appendChild(cardGender);    
+      card.appendChild(cardSpecies);
+    }
+  }
 
 window.onload = async () => {
   await traerDatos();
